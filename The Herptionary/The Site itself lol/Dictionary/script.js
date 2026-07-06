9e53727907478ea7274372160619aa75f300ca24
FrogDude7856 <max.walthamsajdak@gmail.com>
2026-07-06 18:15:53 -0400

    Fix: Formatting

--- The Herptionary/The Site itself lol/Dictionary/script.js
+++ The Herptionary/The Site itself lol/Dictionary/script.js
@@ -28,7 +28,7 @@ btn.addEventListener("click", () => {
             sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
         })
         .catch(() => {
-            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+            result.innerHTML = `<h3 class="error">Could not find this term. Please check your spelling and try again.</h3>`;
         });
 });
 function playSound() {
